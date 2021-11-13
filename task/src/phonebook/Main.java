461a1494451d1686ed2cadc09db1c129e9128015
Jeremy Grecu <jeremy.grecu@sap.com>
2021-11-13 10:25:59 +0000

    JBA - Phone Book - refactor, split into methods, search in lists, cut processing time in half

--- task/src/phonebook/Main.java
+++ task/src/phonebook/Main.java
@@ -7,51 +7,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static long linearSearchTime;
+    private static long linearSearchTimeTemp = 1653109967298L;
+    private static long sortingTime;
+    private static long jumpSearchTime;
     public static void main(String[] args) {
         String pathToFile = "/Users/i337469/Downloads/find.txt";
         String pathSearchFile = "/Users/i337469/Downloads/directory.txt";
-        /*File file = new File(pathToFile);
-        File searchFile = new File(pathSearchFile);
-        int counter = 0;
-        int foundCounter = 0;
-        try (Scanner scanner = new Scanner(file)) {
-            System.out.println("Start searching...");
-            long timeStart = System.currentTimeMillis();
-            while (scanner.hasNextLine()) {
-                String toFind = scanner.nextLine();
-                counter++;
-                try (Scanner sc = new Scanner(searchFile)) {
-                    while (sc.hasNextLine()) {
-                        if (sc.nextLine().strip().split("\\s+", 2)[1].equals(toFind)) {
-                            foundCounter++;
-                            break;
-                        }
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.printf("File not found: %s", pathSearchFile);
-                }
-            }
-            long timeEnd = System.currentTimeMillis();
-
-            System.out.printf("Found %d / %d entries. %s%n", foundCounter, counter, calculateTimeTaken(timeStart, timeEnd));
-        } catch (FileNotFoundException e) {
-            System.out.printf("File not found: %s", pathToFile);
-        }*/
 
         List<String> find = readFileToList(pathToFile);
         List<String> directory = readFileToList(pathSearchFile);
 
-        linearSearch(directory, find);
-
-        System.out.println();
+        System.out.println("Start searching (linear search)...");
+        long foundLinear = linearSearch(directory, find);
+        System.out.printf("Found %d / %d entries. Time taken: %s%n%n", foundLinear, find.size(), calculateTimeTaken(linearSearchTime));
+        
+        System.out.println("Start searching (bubble sort + jump search)...");
+        List<String> sortedDirectory = sortList(directory);
+        if (!sortedDirectory.isEmpty()) {
+            long foundJump = jumpSearchList(sortedDirectory, find);
+            System.out.printf("Found %d / %d entries. Time taken: %s%n", foundJump, find.size(), calculateTimeTaken(jumpSearchTime + sortingTime));
+            System.out.printf("Sorting time: %s%n", calculateTimeTaken(sortingTime));
+            System.out.printf("Searching time: %s%n", calculateTimeTaken(jumpSearchTime));
+        } else {
+            foundLinear = linearSearch(directory, find);
+            System.out.printf("Found %d / %d entries. Time taken: %s%n", foundLinear, find.size(), calculateTimeTaken(linearSearchTime + sortingTime));
+            System.out.printf("Sorting time: %s - STOPPED, moved to linear search%n", calculateTimeTaken(sortingTime));
+            System.out.printf("Searching time: %s%n", calculateTimeTaken(linearSearchTime));
+        }
     }
 
-    private static String calculateTimeTaken(long start, long end) {
-        long timeDifference = end - start;
+    private static String calculateTimeTaken(long timeDifference) {
         long minutes = (timeDifference / 1000) / 60;
         long seconds = (timeDifference / 1000) % 60;
         long milliseconds = timeDifference - ((minutes * 60000) + (seconds * 1000));
-        return "Time taken: " + minutes + " min. " + seconds + " sec. " + milliseconds + "ms.";
+        return minutes + " min. " + seconds + " sec. " + milliseconds + "ms.";
     }
 
     private static List<String> readFileToList(String path) {
@@ -68,10 +58,10 @@ public class Main {
         return list;
     }
 
-    private static void linearSearch(List<String> directory, List<String> find) {
-        System.out.println("Start searching (linear search)...");
+    private static long linearSearch(List<String> directory, List<String> find) {
+
         long timeStart = System.currentTimeMillis();
-        int found = 0;
+        long found = 0;
         for (String toFind : find) {
             for (String entry : directory) {
                 if (entry.strip().split("\\s+", 2)[1].equals(toFind)) {
@@ -81,6 +71,69 @@ public class Main {
             }
         }
         long timeEnd = System.currentTimeMillis();
-        System.out.println("Found " + found + " / " + find.size() + " entries. " + calculateTimeTaken(timeStart, timeEnd));
+        linearSearchTime = timeEnd - timeStart;
+        return found;
+    }
+
+    private static long jumpSearchList(List<String> directory, List<String> find) {
+        long jumpTimeStart = System.currentTimeMillis();
+        long found = 0;
+
+
+        for (String toFind : find) {
+            long result = jumpSearchElement(directory, toFind);
+            if (result == 1) {
+                found++;
+            }
+        }
+        long jumpTimeStop = System.currentTimeMillis();
+        jumpSearchTime = jumpTimeStop - jumpTimeStart;
+        return found;
+    }
+
+    private static long jumpSearchElement(List<String> list, String element) {
+        int size = list.size();
+        int block = (int) Math.floor(Math.sqrt(size));
+        if (element.compareTo(list.get(size - 1).split("\\s+", 2)[1]) > 0 || element.compareTo(list.get(0).split("\\s+", 2)[1]) < 0) {
+            return -1;
+        }
+        int i = 0;
+        int j = block;
+        while (list.get(j).split("\\s+", 2)[1].compareTo(element) < 0 && j < size) {
+            i = j;
+            j = j + block;
+            if (j > size - 1) {
+                j = size - 1;
+            }
+        }
+        for (int k = i; k <= j; k++) {
+            if (list.get(k).split("\\s+", 2)[1].compareTo(element) == 0) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    private static List<String> sortList(List<String> directory) {
+        long sortingTimeStart = System.currentTimeMillis();
+        List<String> list = new ArrayList<>(directory);
+        String temp;
+        for (int i = 0; i < directory.size(); i++) {
+            for (int j = i + 1; j < directory.size(); j++) {
+                long tempTimeDifference = System.currentTimeMillis() - sortingTimeStart;
+                if (tempTimeDifference > (10 * linearSearchTimeTemp)) {
+                    sortingTime = tempTimeDifference;
+                    return new ArrayList<>();
+                }
+                if (directory.get(i).split("\\s+", 2)[1].compareTo(directory.get(j).split("\\s+", 2)[1]) > 0) {
+                    temp =  directory.get(i);
+                    directory.set(i, directory.get(j));
+                    directory.set(j, temp);
+                }
+            }
+        }
+        long sortingTimeStop = System.currentTimeMillis();
+        sortingTime = sortingTimeStop - sortingTimeStart;
+        return list;
     }
 }
