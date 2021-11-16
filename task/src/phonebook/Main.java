57aa2daa5de0fd3ee6b75601abeedb72e60627ce
Jeremy Grecu <jeremy.grecu@sap.com>
2021-11-16 15:00:59 +0000

    JBA - Phone Book - Stage 3/4: Need for speed - created the quickSort

--- task/src/phonebook/Main.java
+++ task/src/phonebook/Main.java
@@ -10,6 +10,7 @@ public class Main {
     private static long linearSearchTime;
     private static long sortingTime;
     private static long jumpSearchTime;
+    private static long binarySearchTime;
     public static void main(String[] args) {
         String pathToFile = "/Users/i337469/Downloads/find.txt";
         String pathSearchFile = "/Users/i337469/Downloads/directory.txt";
@@ -17,23 +18,40 @@ public class Main {
         List<String> find = readFileToList(pathToFile);
         List<String> directory = readFileToList(pathSearchFile);
 
-        System.out.println("Start searching (linear search)...");
-        long foundLinear = linearSearch(directory, find);
-        System.out.printf("Found %d / %d entries. Time taken: %s%n%n", foundLinear, find.size(), calculateTimeTaken(linearSearchTime));
+//        performLinearSearch(find, directory);
+//        performBubbleSortAndJumpOrLinearSearch(find, directory);
+        performQuickSortAndBinarySearch(find, directory);
+    }
 
+    private static void performQuickSortAndBinarySearch(List<String> find, List<String> directory) {
+        System.out.println("Start searching (quick sort + binary search)...");
+        List<String> sortedDirectory = quickSortList(directory);
+        long foundBinary = binarySearchList(sortedDirectory, find);
+        System.out.printf("Found %d / %d entries. Time taken: %s%n", foundBinary, find.size(), calculateTimeTaken(jumpSearchTime + sortingTime));
+        System.out.printf("Sorting time: %s%n", calculateTimeTaken(sortingTime));
+        System.out.printf("Searching time: %s%n%n", calculateTimeTaken(binarySearchTime));
+    }
+
+    private static void performBubbleSortAndJumpOrLinearSearch(List<String> find, List<String> directory) {
         System.out.println("Start searching (bubble sort + jump search)...");
         List<String> sortedDirectory = bubbleSortList(directory);
         if (!sortedDirectory.isEmpty()) {
             long foundJump = jumpSearchList(sortedDirectory, find);
             System.out.printf("Found %d / %d entries. Time taken: %s%n", foundJump, find.size(), calculateTimeTaken(jumpSearchTime + sortingTime));
             System.out.printf("Sorting time: %s%n", calculateTimeTaken(sortingTime));
-            System.out.printf("Searching time: %s%n", calculateTimeTaken(jumpSearchTime));
+            System.out.printf("Searching time: %s%n%n", calculateTimeTaken(jumpSearchTime));
         } else {
-            foundLinear = linearSearch(directory, find);
+            long foundLinear = linearSearch(directory, find);
             System.out.printf("Found %d / %d entries. Time taken: %s%n", foundLinear, find.size(), calculateTimeTaken(linearSearchTime + sortingTime));
             System.out.printf("Sorting time: %s - STOPPED, moved to linear search%n", calculateTimeTaken(sortingTime));
-            System.out.printf("Searching time: %s%n", calculateTimeTaken(linearSearchTime));
+            System.out.printf("Searching time: %s%n%n", calculateTimeTaken(linearSearchTime));
         }
+    }
+
+    private static void performLinearSearch(List<String> find, List<String> directory) {
+        System.out.println("Start searching (linear search)...");
+        long foundLinear = linearSearch(directory, find);
+        System.out.printf("Found %d / %d entries. Time taken: %s%n%n", foundLinear, find.size(), calculateTimeTaken(linearSearchTime));
     }
 
     private static String calculateTimeTaken(long timeDifference) {
@@ -90,6 +108,39 @@ public class Main {
         return found;
     }
 
+    private static long jumpSearchElement(List<String> list, String element) {
+        int listSize = list.size();
+        int jumpBlock = (int) Math.floor(Math.sqrt(listSize));
+        if (element.compareTo(list.get(listSize - 1).split("\\s+", 2)[1]) > 0 ||
+                element.compareTo(list.get(0).split("\\s+", 2)[1]) < 0) {
+            return -1;
+        }
+        int startPosition = 0;
+        int currentPosition = jumpBlock;
+        while (list.get(currentPosition).split("\\s+", 2)[1].compareTo(element) < 0) {
+            startPosition = currentPosition;
+            currentPosition += jumpBlock;
+            if (currentPosition > listSize - 1) {
+                currentPosition = listSize - 1;
+            }
+        }
+        for (int k = startPosition; k <= currentPosition; k++) {
+            if (list.get(k).split("\\s+", 2)[1].compareTo(element) == 0) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    private static long binarySearchList(List<String> directory, List<String> find) {
+        long binaryTimeStart = System.currentTimeMillis();
+        long found = 0;
+        // Algo
+        long binaryTimeStop = System.currentTimeMillis();
+        binarySearchTime = binaryTimeStop - binaryTimeStart;
+        return found;
+    }
+
     private static List<String> bubbleSortList(List<String> directory) {
         long sortingTimeStart = System.currentTimeMillis();
         List<String> list = new ArrayList<>(directory);
@@ -119,28 +170,38 @@ public class Main {
         sortingTime = sortingTimeStop - sortingTimeStart;
         return list;
     }
-    
-    private static long jumpSearchElement(List<String> list, String element) {
-        int listSize = list.size();
-        int jumpBlock = (int) Math.floor(Math.sqrt(listSize));
-        if (element.compareTo(list.get(listSize - 1).split("\\s+", 2)[1]) > 0 ||
-                element.compareTo(list.get(0).split("\\s+", 2)[1]) < 0) {
-            return -1;
+
+    private static List<String> quickSortList(List<String> directory) {
+        long sortingTimeStart = System.currentTimeMillis();
+        List<String> list = quickSort(directory);
+        long sortingTimeStop = System.currentTimeMillis();
+        sortingTime = sortingTimeStop - sortingTimeStart;
+        return list;
+    }
+
+    private static List<String> quickSort(List<String> list) {
+        if (list.isEmpty()) {
+            return list;
         }
-        int startPosition = 0;
-        int currentPosition = jumpBlock;
-        while (list.get(currentPosition).split("\\s+", 2)[1].compareTo(element) < 0) {
-            startPosition = currentPosition;
-            currentPosition += jumpBlock;
-            if (currentPosition > listSize - 1) {
-                currentPosition = listSize - 1;
+        List<String> sorted;
+        List<String> smaller = new ArrayList<>();
+        List<String> greater = new ArrayList<>();
+        String pivot = list.get(list.size() - 1);
+        String j;
+        for (int i = 0; i < list.size() - 1; i++) {
+            j = list.get(i);
+            if (j.split("\\s+", 2)[1].compareTo(pivot.split("\\s+", 2)[1]) < 0) {
+                smaller.add(j);
+            } else {
+                greater.add(j);
             }
         }
-        for (int k = startPosition; k <= currentPosition; k++) {
-            if (list.get(k).split("\\s+", 2)[1].compareTo(element) == 0) {
-                return 1;
-            }
-        }
-        return -1;
+        smaller = quickSort(smaller);
+        greater = quickSort(greater);
+        smaller.add(pivot);
+        smaller.addAll(greater);
+        sorted = smaller;
+
+        return sorted;
     }
 }
